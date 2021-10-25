d644b51210136e1396046a3b42649f0773c7d180
Filip Budiša <filip@circuitmess.com>
2021-10-25 15:26:01 +0200

    Remove leftover ByteBoi splash color values

--- src/ByteBoi.h
+++ src/ByteBoi.h
@@ -83,17 +83,6 @@ private:
 
 	void (* splashCallback)() = nullptr;
 	uint32_t splashTime = 0;
-	uint32_t splashIndex = 0;
-	const RGBColor splashValues[8] = {
-			{ 46, 100, 0 },
-			{ 255, 30, 30 },
-			{ 200, 200, 30 },
-			{ 115, 30, 200 },
-			{ 30, 235, 50 },
-			{ 120, 30, 30 },
-			{ 230, 230, 30 },
-			{ 55, 115, 220 },
-	};
 };
 
 extern ByteBoiImpl ByteBoi;
